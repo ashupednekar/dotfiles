6d50ad8d4585b72fcd2e25c4ca9a45e9111e1f35
ashupednekar <ashutoshpednekar15@gmail.com>
2025-04-26 12:33:00 +0530

    feat: updated trouble conf for inline reports

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,6 +1,22 @@
 return {
   "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  opts = {
+    modes = {
+      preview_float = {
+        mode = "diagnostics",
+        preview = {
+          type = "float",
+          relative = "editor",
+          border = "rounded",
+          title = "Preview",
+          title_pos = "center",
+          position = { 0, -2 },
+          size = { width = 0.3, height = 0.3 },
+          zindex = 200,
+        },
+      },
+    },
+  },
   cmd = "Trouble",
   keys = {
     {
@@ -34,4 +50,31 @@ return {
       desc = "Quickfix List (Trouble)",
     },
   },
+  config = function()
+    require("trouble").setup {
+      -- your existing opts if any
+    }
+
+    -- Inline diagnostics
+    vim.diagnostic.config({
+      virtual_text = true,       -- inline text
+      signs = true,              -- signs in the gutter
+      underline = true,          -- underline the error line
+      update_in_insert = false,  -- don't show diagnostics while typing
+      severity_sort = true,
+      float = {
+        border = "rounded",
+        source = "always",
+        header = "",
+        prefix = "",
+      },
+    })
+
+    -- Optional: show floating diagnostics on CursorHold
+    vim.api.nvim_create_autocmd("CursorHold", {
+      callback = function()
+        vim.diagnostic.open_float(nil, { focus = false })
+      end,
+    })
+  end,
 }
