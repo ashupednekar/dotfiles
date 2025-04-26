22e920ea954187a3f0b094c7206a5e2aa782e98e
ashupednekar <ashutoshpednekar15@gmail.com>
2025-04-26 12:56:57 +0530

    fix: disabled arrow keys

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -1,6 +1,22 @@
 -- disable mouse
 vim.o.mouse = ""
 
+-- Disable arrow keys in normal, insert, and visual modes
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<Left>', '<Nop>', opts)
+vim.keymap.set('n', '<Right>', '<Nop>', opts)
+vim.keymap.set('n', '<Up>', '<Nop>', opts)
+vim.keymap.set('n', '<Down>', '<Nop>', opts)
+vim.keymap.set('i', '<Left>', '<Nop>', opts)
+vim.keymap.set('i', '<Right>', '<Nop>', opts)
+vim.keymap.set('i', '<Up>', '<Nop>', opts)
+vim.keymap.set('i', '<Down>', '<Nop>', opts)
+vim.keymap.set('v', '<Left>', '<Nop>', opts)
+vim.keymap.set('v', '<Right>', '<Nop>', opts)
+vim.keymap.set('v', '<Up>', '<Nop>', opts)
+vim.keymap.set('v', '<Down>', '<Nop>', opts)
+
+
 --telescope
 local telescope = require('telescope.builtin')
 vim.keymap.set('n', '<leader><leader>', telescope.find_files, {})
