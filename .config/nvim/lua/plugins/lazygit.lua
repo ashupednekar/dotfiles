df6ea1de098c33a9f3efdb0bb9cd243f35eea9ef
ashupednekar <ashutoshpednekar15@gmail.com>
2025-04-26 14:09:51 +0530

    chore: added more lg command

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -15,6 +15,8 @@ return {
   -- setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-    { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    { "<leader>g", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    { "<leader>gl", "<cmd>LazyGitFilter<cr>", desc = "LazyGit - Log/Filter" },
+    { "<leader>gL", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "LazyGit - Log Current File" },
   }
 }
