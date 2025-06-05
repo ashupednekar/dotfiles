a4b085ed15c02cc9bd5e330ba397d379164cca90
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-05 08:34:36 +0530

    feat: added mac install scripts

---
+++ scripts/macsetup.sh
@@ -0,0 +1,21 @@
+#main stuff
+brew install zen-browser
+brew install nvim
+brew install --cask ghostty
+brew install koekeishiya/formulae/yabai
+brew install koekeishiya/formulae/skhd
+
+#sketchybar
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install lua
+brew tap FelixKratz/formulae
+brew install sketchybar
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+
+# Fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+brew install --cask font-jetbrains-mono
+
