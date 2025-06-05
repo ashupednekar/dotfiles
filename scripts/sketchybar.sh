283e57ad237d0638a0b941ad5783c990261af6f5
ashupednekar <ashutoshpednekar15@gmail.com>
2025-06-05 06:39:54 +0530

    chore: added sketchybar dependency script

---
+++ scripts/sketchybar.sh
@@ -0,0 +1,13 @@
+brew install lua
+brew install switchaudio-osx
+brew install nowplaying-cli
+
+brew tap FelixKratz/formulae
+brew install sketchybar
+
+# Fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
