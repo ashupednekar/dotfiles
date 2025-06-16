ac04ac72857e76479e9d26aface427879186a6b8
ashu work <ashu@Ashus-Work-MacBook-Air.local>
2025-06-16 13:41:17 +0530

    chore: updated mac setup script

--- scripts/macsetup.sh
+++ scripts/macsetup.sh
@@ -5,14 +5,11 @@ brew install --cask ghostty
 brew install koekeishiya/formulae/yabai
 brew install koekeishiya/formulae/skhd
 
-#sketchybar
-brew tap FelixKratz/formulae
-brew install sketchybar
+# Languages 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+brew install go
+brew install python@3.11
 brew install lua
-brew install awscli
-brew tap FelixKratz/formulae
-brew install sketchybar
-curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 
 # Fonts
 brew install --cask sf-symbols
@@ -20,15 +17,18 @@ brew install --cask font-sf-mono
 brew install --cask font-sf-pro
 brew install --cask font-jetbrains-mono
 
-# Languages 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
-brew install go
-brew install python@3.11
-
 # Misc 
 cargo install starship
 cargo install zoxide
 go install github.com/jesseduffield/lazygit@latest
 brew install --cask microsoft-teams
 brew install --cask rancher
+brew tap FelixKratz/formulae
+brew install sketchybar
+brew install awscli
+brew tap FelixKratz/formulae
+brew install sketchybar
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+brew install helm
+brew install kubectl
 
