1a4d6e9fbd5a6e0cda985cb9e125095bafc5d4e8
ashupednekar <ashutoshpednekar15@gmail.com>
2025-10-14 14:51:29 +0530

    chore: added sketchybar config to conf

--- scripts/macsetup.sh
+++ scripts/macsetup.sh
@@ -26,11 +26,31 @@ go install github.com/jesseduffield/lazygit@latest
 brew install --cask microsoft-teams
 brew install --cask rancher
 brew tap FelixKratz/formulae
-brew install sketchybar
 brew install awscli
-brew tap FelixKratz/formulae
 brew install sketchybar
 curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v2.0.28/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
 brew install helm
 brew install kubectl
+
+#Sketchybar
+osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to true'
+osascript -e 'tell application "System Events" to set autohide menu bar of dock preferences to false'
+brew tap FelixKratz/formulae
+brew install sketchybar
+mkdir -p ~/.config/sketchybar/plugins
+cp /opt/homebrew/opt/sketchybar/share/sketchybar/examples/sketchybarrc ~/.config/sketchybar/sketchybarrc
+cp -r /opt/homebrew/opt/sketchybar/share/sketchybar/examples/plugins/ ~/.config/sketchybar/plugins/
+chmod +x ~/.config/sketchybar/plugins/*
+brew services restart felixkratz/formulae/sketchybar
+brew services start sketchybar
+sketchybar --reload
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+brew install --cask font-meslo-lg-nerd-font
+brew install --cask sf-symbols
+brew install jq
+brew install gh
+brew install switchaudio-osx
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.23/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+sketchybar --reload
 
