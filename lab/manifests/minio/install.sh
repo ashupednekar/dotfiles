c6736a491e6fdf200429949f1997349542e38e45
ashupednekar <ashutoshpednekar15@gmail.com>
2025-11-21 05:12:10 +0530

    chore: sync

--- lab/manifests/minio/install.sh
+++ lab/manifests/minio/install.sh
@@ -1,2 +1,2 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
-helm upgrade --install minio bitnami/minio -f minio.yaml
+helm upgrade --install minio bitnami/minio -f values.yaml -n essentials
