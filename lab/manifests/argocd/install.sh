23dc6f1235f198d38ef91163146bd9acf75bdea4
ashupednekar <ashutoshpednekar15@gmail.com>
2025-12-13 13:50:49 +0530

    chore: moved lab to its own repo

--- lab/manifests/argocd/install.sh
+++
@@ -1,1 +0,0 @@
-helm install cd -n argocd --create-namespace oci://ghcr.io/argoproj/argo-helm/argo-cd --version 8.5.10 -f values.yaml
--- lab/manifests/minio/install.sh
+++
@@ -1,2 +0,0 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm upgrade --install minio bitnami/minio -f values.yaml -n essentials
